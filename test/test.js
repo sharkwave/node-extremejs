a55b5cf955e496509934da2da788fad963cb6923
liudian <theseus.liu@gmail.com>
2011-10-25 08:18:18 +0800

    test new demo

--- test/test.js
+++ test/test.js
@@ -25,10 +25,18 @@ xp.entity('favorite', {
   spot:'spot'
 });
 
-xp.const('home', [], {
-  signup: "/user",
-  login: "/login"
+xp.resource('home', [], {
+  signup: ['signup', []],
+  login: ['login', []]
 });
+
+xp.stream('signup', 'user', []);
+
+xp.object('user-by-name', 'user', ['username']);
+
+xp.resource('login', [], function(req, callback) {
+});
+
 xp.stream('find-friend', 'user', ['currentUser']);
 
 xp.stream('following', 'friend', ['currentUser', 'from'], ['to']);
@@ -43,5 +51,12 @@ xp.object('save', 'favorite', ['user', 'spot']);
 
 xp.connect('localhost', 27017, 'test', function(err) {
   if(err) console.log('error%j', err);
-  xp.test();
+  //xp.test();
+setTimeout(function() {
+  console.log('here');
+  xp.post("/signup", {username:'liudian', password:'12345'}, function(err,obj){
+    console.log(err);
+    console.log(obj);
+  });
+}, 1000);
 });
